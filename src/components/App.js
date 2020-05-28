5a49560defbcd41d3a190bd84810bf730c7c97ed
Ansh Mehta <46837820+mehtaAnsh@users.noreply.github.com>
2020-05-28 18:15:45 +0530

    Showed flash screen, resolved device detection err

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,98 @@
-import React from 'react';
-//import FlareComponent from 'flare-react';
-import Navbar from './Navbar';
-import '../static/homepage.css';
+import React from "react";
+import FlareComponent from "flare-react";
+import Navbar from "./Navbar";
+import { BrowserView, MobileView } from "react-device-detect";
+import "../static/homepage.css";
 
-const App = () => {
-    return (
-        <div>
-            <div>                
-                <Navbar />                
-                <div className="container" id="main">
-                    <div className="row boxx">
-                        <div className="text">
-                            <img src="../assets/vl-text.png" style={{maxWidth: '70%'}}/>
-                            <b><p>is based on the idea that lab experiments <br/>
-                            can be taught using the Internet, more efficiently and less<br/>
-                            expensively. The labs can also made be more available to students <br/>
-                            with no access to physical labs or where equipment <br/>
-                            is not available owing to being scarce or costly. 
-                            </p></b>                
-                        </div>            
-                        <img src="./assets/lab.gif" alt="image" className="img-fluid float-right" />                  
-                    </div>
-                    <div className="row">
-                        <h1 style={{marginLeft: '47%', marginBottom: '10px', backgroundColor: '#95aedd', borderRadius: '10px'}}>&nbsp; EXPERIMENTS &nbsp;</h1>
-                    </div>
-                    <div className="container row" style={{marginLeft: '-3%'}}>
-                        <div className="col-1 stds">
-                            <p style={{paddingLeft: '30%', backgroundColor: '#fcabc2', borderRadius: '10px'}}>S<br/>T<br/>A<br/>N<br/>D<br/>A<br/>R<br/>D<br/></p>
-                        </div>
-                        <div className="col-1">
-                            <div className="row levels">
-                                <h4 className="text">VII</h4>
-                            </div>
-                            <div className="row levels">
-                                <h4 className="text">VIII</h4>
-                            </div>
-                            <div className="row levels">
-                                <h4 className="text">IX</h4>
-                            </div>
-                            <div className="row levels">
-                                <h4 className="text">X</h4>
-                            </div>
-                        </div>
-                        <div className="col-lg-10" style={{marginRight: '-5%'}}>
-                            <div className="row">
-                                <div className="col-lg-5 card card-1">
-                                    <h2>1. Growing plants from seeds</h2>                            
-                                    <div id="root"></div>
-                                </div>
-                                <div className="col-lg-5 card card-2">
-                                    <h2>2. Growing plants from seeds</h2>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-lg-5 card card-3">
-                                    <h2>3. Growing plants from seeds</h2>
-                                </div>
-                                <div className="col-lg-5 card card-4">
-                                    <h2>4. Growing plants from seeds</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/><br/>
-                    {/*
+class App extends React.Component {
+	state = { loading: true };
+
+	componentDidMount() {
+		this.loader().then(() => this.setState({ loading: false }));
+	}
+
+	loader() {
+		return new Promise(resolve => setTimeout(() => resolve(), 5000));
+	}
+
+	render() {
+		if (this.state.loading) {
+			return (
+				<div style={{ marginTop: "-7.5%", marginLeft: "15%" }}>
+					<FlareComponent file="rive_files/logo.flr" animationName="logo" width={1000} height={1000} transparent="true" />
+				</div>
+			);
+		}
+		return (
+			<div>
+				<BrowserView>
+					<Navbar />
+					<div className="container" id="main">
+						<div className="row boxx">
+							<div className="text">
+								<img src="../assets/vl-text.png" style={{ maxWidth: "70%" }} alt="Virtual Labs" />
+								<b>
+									<p>
+										is based on the idea that lab experiments <br />
+										can be taught using the Internet, more efficiently and less
+										<br />
+										expensively. The labs can also made be more available to students <br />
+										with no access to physical labs or where equipment <br />
+										is not available owing to being scarce or costly.
+									</p>
+								</b>
+							</div>
+							<div className="img-fluid float-right">
+								<FlareComponent file="rive_files/lab.flr" animationName="lab" transparent="true" width={600} height={350} />
+							</div>
+						</div>
+						<div className="row">
+							<h1 style={{ marginLeft: "47%", marginBottom: "10px", backgroundColor: "#95aedd", borderRadius: "10px" }}>&nbsp; EXPERIMENTS &nbsp;</h1>
+						</div>
+						<div className="container row" style={{ marginLeft: "-3%" }}>
+							<div className="col-1 stds">
+								<p style={{ paddingLeft: "30%", backgroundColor: "#fcabc2", borderRadius: "10px" }}>
+									S<br />T<br />A<br />N<br />D<br />A<br />R<br />D<br />
+								</p>
+							</div>
+							<div className="col-1">
+								<div className="row levels">
+									<h4 className="text">VII</h4>
+								</div>
+								<div className="row levels">
+									<h4 className="text">VIII</h4>
+								</div>
+								<div className="row levels">
+									<h4 className="text">IX</h4>
+								</div>
+								<div className="row levels">
+									<h4 className="text">X</h4>
+								</div>
+							</div>
+							<div className="col-lg-10" style={{ marginRight: "-5%" }}>
+								<div className="row">
+									<div className="col-lg-5 card card-1">
+										<h2>1. Growing plants from seeds</h2>
+										<div id="root"></div>
+									</div>
+									<div className="col-lg-5 card card-2">
+										<h2>2. Growing plants from seeds</h2>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-lg-5 card card-3">
+										<h2>3. Growing plants from seeds</h2>
+									</div>
+									<div className="col-lg-5 card card-4">
+										<h2>4. Growing plants from seeds</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br />
+						<br />
+						<br />
+						{/*
                     <div className="row">
                         <h1 className="text-center" style=margin-left: 47%; margin-bottom: 10px; background-color: #95aedd; border-radius: 10px;">&nbsp; ACTIVITIES &nbsp;</h1>
                     </div>
@@ -124,22 +153,28 @@ const App = () => {
                                 </div>
                             </div>
                         </div>                
-                    </div>     */}           
-                </div>
-                <div className="container" id="mobile">
-                    <img src="./assets/vl-logo.png" alt="Virtual Labs" style={{maxHeight: '50%', maxWidth: '100%', marginLeft: '-5%'}}/>
-                    <div style={{marginLeft: '2%'}}>
-                        <h3>is available now on the Play Store and App Store!</h3>
-                        <h6>Click to download the app now.</h6>
-                    </div>  
-                    
-                    <a href="" id="play-store">   <img src="./assets/play_store.svg" alt="Download from Play Store" width="275" height="100" /> </a>
-                    <a href="" id="app-store">   <img src="./assets/app_store.svg" alt="Download from App Store" width="275" height="100" /> </a>
-                
-                </div>    
-            </div>
-        </div>
-    ); 
+                    </div>     */}
+					</div>
+				</BrowserView>
+				<MobileView>
+					<div className="container" id="mobile">
+						<img src="./assets/vl-logo.png" alt="Virtual Labs" style={{ maxHeight: "50%", maxWidth: "100%", marginLeft: "-5%" }} />
+						<div style={{ marginLeft: "2%" }}>
+							<h3>is available now on the Play Store and App Store!</h3>
+							<h6>Click to download the app now.</h6>
+						</div>
+
+						<a href="" id="play-store">
+							<img src="./assets/play_store.svg" alt="Download from Play Store" width="275" height="100" />{" "}
+						</a>
+						<a href="" id="app-store">
+							<img src="./assets/app_store.svg" alt="Download from App Store" width="275" height="100" />{" "}
+						</a>
+					</div>
+				</MobileView>
+			</div>
+		);
+	}
 }
 
 export default App;
