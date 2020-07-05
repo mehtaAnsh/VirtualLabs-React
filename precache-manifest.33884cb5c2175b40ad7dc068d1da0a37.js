93effdd8022826a953c387c25d017213dd0d7f2d
Ansh Mehta <46837820+mehtaAnsh@users.noreply.github.com>
2020-07-05 14:13:36 +0530

    Updates

---
+++ precache-manifest.33884cb5c2175b40ad7dc068d1da0a37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e334565ba80b0a43b7eec072386e2d24",
+    "url": "/mehtaAnsh/VirtualLabs-Reactindex.html"
+  },
+  {
+    "revision": "a148b8e425c48cf7f42b",
+    "url": "/mehtaAnsh/VirtualLabs-Reactstatic/css/main.4037ce2a.chunk.css"
+  },
+  {
+    "revision": "e4e0d9bf394f24beb292",
+    "url": "/mehtaAnsh/VirtualLabs-Reactstatic/js/2.6746fead.chunk.js"
+  },
+  {
+    "revision": "966d98d911b4226544fb08c654ad9e54",
+    "url": "/mehtaAnsh/VirtualLabs-Reactstatic/js/2.6746fead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a148b8e425c48cf7f42b",
+    "url": "/mehtaAnsh/VirtualLabs-Reactstatic/js/main.0d248dfd.chunk.js"
+  },
+  {
+    "revision": "40e09a75165613362d2e",
+    "url": "/mehtaAnsh/VirtualLabs-Reactstatic/js/runtime-main.0d539e3b.js"
+  },
+  {
+    "revision": "fa7f5e6ad471e01d6231e00ae0fc7f23",
+    "url": "/mehtaAnsh/VirtualLabs-Reactstatic/media/dustismo_bold.fa7f5e6a.ttf"
+  }
+]);
