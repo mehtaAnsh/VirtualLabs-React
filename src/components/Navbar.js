a2cef3b275751d7114a5b0179a9a4f1df10cfb12
Ansh Mehta <46837820+mehtaAnsh@users.noreply.github.com>
2020-08-27 15:38:28 +0530

    Navbar renamed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../static/Navbar.css';
+import '../static/navbar.css';
 
 class Navbar extends React.Component {
 	renderNavbar() {
